a54daf7d5dc4d788fbb674fb918b73f4a75f6370
niko-cxvedadze <nikolozcxvedadze@gmail.com>
2023-12-15 20:59:21 +0400

    fix(remove-unecessary-things)

--- app/subjects/components/Subject/Subject.tsx
+++ app/subjects/components/Subject/Subject.tsx
@@ -1,15 +1,11 @@
 import { ListItem } from "@rneui/base";
 
-import { TSubject } from "../../../../data/subjects";
-
-type SubjectProps = { subject: TSubject };
-
-export function Subject({ subject }: SubjectProps) {
+export function Subject() {
   return (
     <ListItem.Accordion
       content={
         <ListItem.Content>
-          <ListItem.Title>{subject.title}</ListItem.Title>
+          <ListItem.Title></ListItem.Title>
         </ListItem.Content>
       }
     ></ListItem.Accordion>
--- app/subjects/index.tsx
+++ app/subjects/index.tsx
@@ -1,12 +1,7 @@
-import { subjects } from "../../data/subjects";
-import { Subject } from "./components/Subject/Subject";
-
 export default function index() {
-  return (
-    <>
-      {subjects?.map((subject) => {
-        return <Subject key={subject.id} subject={subject} />;
-      })}
-    </>
-  );
+    return (
+        <>
+
+        </>
+    );
 }
