90802c568b6f7fdc62de8cc66df50e272efc9346
niko-cxvedadze <nikolozcxvedadze@gmail.com>
2023-12-21 22:34:37 +0400

    remove unnecessary garbage

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,8 @@
 import { View, Text } from "react-native";
-import { Link } from "expo-router";
 
 export default function Page() {
   return (
     <View>
-      <Link href={"quizes"}>ქვიზები</Link>
       <Text>ტექსტი</Text>
     </View>
   );
