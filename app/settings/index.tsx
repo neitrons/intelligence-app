04d59fd5fb5d77e19da2dae8cc5ed21063fad37b
niko-cxvedadze <nikolozcxvedadze@gmail.com>
2023-12-24 15:47:47 +0400

    feature(settings-page-setup)

--- app/settings/index.tsx
+++ app/settings/index.tsx
@@ -3,7 +3,7 @@ import { View, Text } from "react-native";
 export default function Settings() {
   return (
     <View>
-      <Text>text</Text>
+      <Text>Settings Page</Text>
     </View>
   );
 }
