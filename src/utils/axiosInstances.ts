4c1ccd48fbe16a59a7dae58b2942cd8a3d39683e
niko-cxvedadze <nikolozcxvedadze@gmail.com>
2024-05-09 21:35:01 +0400

    update

--- src/utils/axiosInstances.ts
+++ src/utils/axiosInstances.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export const axiosGithub = axios.create({
   baseURL: "https://raw.githubusercontent.com/neitrons/intelligence-data/main",
   headers: {
-    Authorization: `token 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`,
+    Authorization: `token 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`,
   },
 });
--- src/view/Standard/StandardFooter/StandardFooter.tsx
+++ src/view/Standard/StandardFooter/StandardFooter.tsx
@@ -22,7 +22,7 @@ export function StandardFooter() {
   } = useStandardProvider();
 
   const { seconds, startTimer, running, stopReset } = useCountDown({
-    secondsAmount: 10,
+    secondsAmount: 60,
   });
 
   const initialMode = useMemo(
